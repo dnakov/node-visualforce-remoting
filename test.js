6e6861bf18e18e8e1aa4cec81f88de4504f6e1f2
daniel nakov <daniel.nakov@gmail.com>
2017-06-07 12:40:46 -0400

    remove console

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
-var VF = require('./index.js')
+// var VF = require('./index.js')
 
-var vf = VF(process.env.SF_SERVER_URL, process.env.SF_USER, process.env.SF_PASSWORD, function(er, executeMethod) {
-  executeMethod('/apex/comm', 'Commissions.getData', ['employees'], function(er, res) {
+// var vf = VF(process.env.SF_SERVER_URL, process.env.SF_USER, process.env.SF_PASSWORD, function(er, executeMethod) {
+//   executeMethod('/apex/comm', 'Commissions.getData', ['employees'], function(er, res) {
+//     console.log(er, res)
+//   })
+// })
+
+var VF = require('./vfproxy')
+
+VF('https://na40.salesforce.com', '00D460000016Itr!ARUAQEcOtnmrnG8i0wAfMDZujpeJml321ld_wpN56fZIZjT2zDBpGzu16cfN4ioByphibAnFWgQsCtg5dOI1sD7E5cgOLyI8', function(er, executeMethod) {
+  executeMethod('/apex/config', 'porthole.config.getApiNames', [], (er, res) => {
     console.log(er, res)
   })
 })
-
--- vfproxy.js
+++ vfproxy.js
@@ -1,20 +1,110 @@
 var request = require('request')
 
-module.exports = function(serverUrl, sessionId, cb) {
+
+function getServerUrl(serverUrl) {
+  let VFURL
+  var firstDotPos = serverUrl.indexOf('.')
+  if(serverUrl.indexOf('my.salesforce') !== -1) {
+    VFURL = serverUrl.substr(0, firstDotPos) + '--c.' + serverUrl.substr(firstDotPos + 1)
+    VFURL = VFURL.replace('my.salesforce', 'visual.force' )
+  } else {
+    VFURL = serverUrl.substr(0, 8) + 'c.' + serverUrl.substr(8)
+    VFURL = VFURL.replace('salesforce', 'visual.force')
+  }
+  return VFURL
+}
+
+function getCookieJar(sessionId, serverUrl) {
+  let JAR = request.jar()
+  var cookie = request.cookie('sid=' + sessionId)
+  JAR.setCookie(cookie, serverUrl)
+  return JAR
+}
+
+module.exports.getVisualforcePage = function(serverUrl, sessionId, vfpage, cb) {
+  var JAR, VFURL, RCONTEXT
+  var PAGE_CACHE = {}
+
+  VFURL = getServerUrl(serverUrl)
+  JAR = getCookieJar(sessionId, serverUrl)
+
+  request({
+    url: serverUrl + vfpage,
+    jar: JAR,
+    method: 'GET'
+  }, function(er, res) {
+    if(er) return cb(er)
+
+
+    VFURL = res.request.uri.protocol + '//' + res.request.uri.host
+    JAR.setCookie(JAR.getCookieString(VFURL))
+    try {
+      var remotingstuff = JSON.parse(res.body.split('Visualforce.remoting.Manager.add(new $VFRM.RemotingProviderImpl(')[1].split('));')[0])
+    } catch(er) {
+      return cb(er)
+    }
+
+    PAGE_CACHE[vfpage] = remotingstuff
+    //return cb(null, executeMethod)
+    return cb(null, remotingstuff)
+  })
+}
+
+module.exports.apexremote = function _executeMethod(serverUrl, sessionId, pageTokens, vfpage, method, data, cb) {
+  let VFURL = getServerUrl(serverUrl)
+  let JAR = getCookieJar(sessionId)
+
+  var lastDot = method.lastIndexOf('.')
+  var cls = method.slice(0, lastDot)
+  var mtd = method.slice(lastDot + 1)
+  var _ctx = pageTokens.actions[cls].ms.filter( function(c) { return c.name === mtd } )
+  var ctx = _ctx.length > 0 && _ctx[0]
+  ctx.vid = pageTokens.vf.vid
+  request({
+    url: VFURL + '/apexremote',
+    method: 'POST',
+    jar: JAR,
+    headers: {
+      Referer: VFURL + vfpage,
+      Origin: VFURL,
+      'X-User-Agent': 'Visualforce-Remoting',
+      DNT: '1',
+
+    },
+    json: true,
+    body: {
+      action: cls,
+      ctx: ctx,
+      method: mtd,
+      type:'rpc',
+      data: data,
+      tid: 2
+    }
+  }, function(er, res) {
+    if(er) return cb(er)
+    cb(null,resolveRefs(res.body[0].result))
+  })
+}
+
+
+module.exports = function main(serverUrl, sessionId, cb) {
 
   var JAR, VFURL, RCONTEXT
   var PAGE_CACHE = {}
 
   var firstDotPos = serverUrl.indexOf('.')
-  VFURL = serverUrl.substr(0, firstDotPos) + '--c.' + serverUrl.substr(firstDotPos + 1)
-  VFURL = VFURL.replace('my.salesforce', 'visual.force' )
-  console.log(VFURL)
+  if(serverUrl.indexOf('my.salesforce') !== -1) {
+    VFURL = serverUrl.substr(0, firstDotPos) + '--c.' + serverUrl.substr(firstDotPos + 1)
+    VFURL = VFURL.replace('my.salesforce', 'visual.force' )
+  } else {
+    VFURL = serverUrl.substr(0, 8) + 'c.' + serverUrl.substr(8)
+    VFURL = VFURL.replace('salesforce', 'visual.force')
+  }
 
   var tok = sessionId
   JAR = request.jar()
   var cookie = request.cookie('sid=' + tok)
   JAR.setCookie(cookie, serverUrl)
-  console.log('initializing')
   cb(null, executeMethod)
   return executeMethod
 
@@ -25,11 +115,8 @@ module.exports = function(serverUrl, sessionId, cb) {
       method: 'GET'
     }, function(er, res) {
       if(er) return cb(er)
-
-      console.log('Parsing VF page: ' + vfpage)
-
+      VFURL = res.request.uri.protocol + '//' + res.request.uri.host
       JAR.setCookie(JAR.getCookieString(VFURL))
-
       try {
         var remotingstuff = JSON.parse(res.body.split('Visualforce.remoting.Manager.add(new $VFRM.RemotingProviderImpl(')[1].split('));')[0])
       } catch(er) {
@@ -43,12 +130,12 @@ module.exports = function(serverUrl, sessionId, cb) {
   }
 
   function _executeMethod(pageTokens, vfpage, method, data, cb) {
-    var cls = method.split('.')[0]
-    var mtd = method.split('.')[1]
+    var lastDot = method.lastIndexOf('.')
+    var cls = method.slice(0, lastDot)
+    var mtd = method.slice(lastDot + 1)
     var _ctx = pageTokens.actions[cls].ms.filter( function(c) { return c.name === mtd } )
     var ctx = _ctx.length > 0 && _ctx[0]
     ctx.vid = pageTokens.vf.vid
-
     request({
       url: VFURL + '/apexremote',
       method: 'POST',
@@ -79,6 +166,7 @@ module.exports = function(serverUrl, sessionId, cb) {
     var pageTokens = PAGE_CACHE[vfpage]
     if(!pageTokens) {
       getPageTokens(vfpage, function(er, pageTokens) {
+        if(er) return cb(er)
          _executeMethod(pageTokens, vfpage, method, data, cb)
       })
     } else {
